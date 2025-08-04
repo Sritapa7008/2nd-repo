c0b582fc65a1468fc7f746625d18b3189e37f8d6
Sritapa7008 <Sritapasahoo7008@gmail.com>
2025-08-04 23:03:41 +0530

    new line

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -5,7 +5,7 @@ public class Rectangle extends Color {
 
     public void area(int l,int b)
     {
-        System.out.println("area is "+" "+l*b);
+        System.out.println("area is    "+"    "+l*b);
     }
 
 }
